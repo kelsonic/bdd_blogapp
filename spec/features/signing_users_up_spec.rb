a9e2ac2d38478674390f4771fbf0691bfc3e1046
kgadams93 <kgadams93@gmail.com>
2016-02-07 17:16:25 +0000

    Style devise signup view

--- spec/features/signing_users_up_spec.rb
+++ spec/features/signing_users_up_spec.rb
@@ -6,11 +6,13 @@ RSpec.feature "Users signup" do
     visit "/"
     
     click_link "Sign Up"
+    fill_in "First name", with: "Joe"
+    fill_in "Last name", with: "Conroy"
     fill_in "Email", with: "user@example.com"
     fill_in "Password", with: "password"
     fill_in "Password confirmation", with: "password"
     
-    click_button "Sign up"
+    click_button "Sign Up"
     
     expect(page).to have_content("You have signed up successfully")
   end
