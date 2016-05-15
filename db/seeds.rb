b3983294d4298f0bf36f54081f3ad2f1e1d5a92d
kgadams93 <kgadams93@gmail.com>
2016-05-15 19:21:42 +0000

    Add seed content

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+20.times do 
+  User.create!(first_name: Faker::Name.first_name, email: Faker::Internet.email, last_name: Faker::Name.last_name, password: "password")
+end
+
+100.times do
+  User.find(rand(1..(User.all.size))).articles.create!(title: "How to #{Faker::Hacker.verb} #{Faker::Hacker.noun}", body: "Don't you want to know how to #{Faker::Hacker.verb} #{Faker::Hacker.noun}, too? Let me know how you think we can do this.")
+end
+
+200.times do
+  Article.find(rand(1..(Article.all.size))).comments.create!(user_id: rand(1..(User.all.size)), body: "#{Faker::Hacker.say_something_smart}")
+end
